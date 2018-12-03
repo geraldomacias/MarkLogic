b986e93475e735934fc5c4746be9ad7c408c6dfb
Spencer Schurk <spencer@joyrun.com>
2018-12-03 11:36:07 -0800

    Working on route handling for users

--- services/users/project/api/users.py
+++ services/users/project/api/users.py
@@ -1,6 +1,9 @@
 # services/users/project/api/users.py
 
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
+
+from project.api.models import User
+from project import db
 
 users_blueprint = Blueprint('users', __name__)
 
@@ -10,3 +13,16 @@ def ping_pong():
         'status': 'success',
         'message': 'pong!'
     })
+
+@users_blueprint.route('/users', methods=['POST'])
+def add_user():
+    post_data = request.get_json()
+    username = post_data.get('username')
+    email = post_data.get('email')
+    db.session.add(User(username=username, email=email))
+    db.session.commit()
+    response_object = {
+        'status': 'success',
+        'message': f'{email} was added!'
+    }
+    return jsonify(response_object), 201
--- services/users/project/tests/test_users.py
+++ services/users/project/tests/test_users.py
@@ -16,5 +16,74 @@ class TestUserService(BaseTestCase):
         self.assertIn('pong!', data['message'])
         self.assertIn('success', data['status'])
 
+    def test_add_user(self):
+        """Ensure a new user can be added to the database."""
+        with self.client:
+            response = self.client.post(
+                '/users',
+                data=json.dumps({
+                    'username': 'spencer',
+                    'email': 'spencer.schurk@gmail.com'
+                }),
+                content_type='application/json',
+            )
+            data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 201)
+            self.assertIn('spencer.schurk@gmail.com was added!', data['message'])
+            self.assertIn('success', data['status'])
+    
+    def test_add_user_invalid_json(self):
+        """Ensure error is thrown if the JSON object is empty."""
+        with self.client:
+            response = self.client.post(
+                '/users',
+                data=json.dumps({}),
+                content_type='application/json',
+            )
+            data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 400)
+            self.assertIn('Invalid payload.', data['message'])
+            self.assertIn('fail', data['status'])
+
+    def test_add_user_invalid_json_keys(self):
+        """Ensure error is thrown if the JSON object does not have a username key."""
+        with self.client:
+            response = self.client.post(
+                '/users',
+                data=json.dumps({
+                    'email': 'spencer.schurk@gmail.com'
+                }),
+                content_type='application/json',
+            )
+            data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 400)
+            self.assertIn('Invalid payload.', data['message'])
+            self.assertIn('fail', data['status'])
+    
+    def test_add_user_duplicate_email(self):
+        """Ensure error is thrown if the email already exists."""
+        with self.client:
+            self.client.post(
+                '/users',
+                data=json.dumps({
+                    'username': 'spencer',
+                    'email': 'spencer.schurk@gmail.com'
+                }),
+                content_type='application/json',
+            )
+            response = self.client.post(
+                '/users',
+                data=json.dumps({
+                    'username': 'spencer',
+                    'email': 'spencer.schurk@gmail.com'
+                }),
+                content_type='application/json',
+            )
+            data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 400)
+            self.assertIn('Sorry. That email already exxists.', data['message'])
+            self.assertIn('fail', data['status'])
+
+
 if __name__ == '__main__':
     unittest.main()
