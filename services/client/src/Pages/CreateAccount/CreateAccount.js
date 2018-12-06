a1d98f4d171017e13f4f5ca3d421fcb2c3cebabf
Matt Yarmolich <mattyarmo@gmail.com>
2018-12-05 18:49:11 -0800

    asfasf

--- services/client/src/Pages/CreateAccount/CreateAccount.js
+++ services/client/src/Pages/CreateAccount/CreateAccount.js
@@ -3,8 +3,23 @@ import './CreateAccount.css';
 import Home from '../Home/Home';
 import { Link } from "react-router-dom";
 import Input from '@material-ui/core/Input';
+import axios from 'axios';
 
 class CreateAccount extends Component {
+  constructor(props){
+    super(props);
+  }
+  submitCreds = (e) => {
+    var credentialJSON = [{
+      username: null, email: null
+    }];
+      axios.post('http://localhost:3000/users', credentialJSON, {
+          headers: {
+            'Content-Type': 'multipart/form-data'
+          }
+      });
+  }
+
   render() {
     return (
       <div className = "login-container">
@@ -12,9 +27,9 @@ class CreateAccount extends Component {
         <Input placeholder = "Username" className = "login"/>
         <Input placeholder = "Email" className = "login"/>
 
-        <Link to="/Home" className="submit">
-          <div className="login-text">Sign Up!</div>
-        </Link>
+        <button onClick={(e) => this.submitCreds(e)} className="login">
+          Sign Up
+        </button>
 
 
         <div className = "footer">
