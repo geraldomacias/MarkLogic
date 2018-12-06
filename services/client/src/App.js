d4ba128eb18eb17ba5894ab506a8eb178daa6732
Matt Yarmolich <mattyarmo@gmail.com>
2018-12-06 00:37:07 -0800

    worked on a bunch of cool shit

--- services/client/src/App.js
+++ services/client/src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
     this.getUsers();
   }
   getUsers() {
-    axios.get(`${process.env.REACT_APP_USERS_SERVICE_URL}users`)
+    axios.get(`${process.env.REACT_APP_USERS_SERVICE_URL}/users`)
     .then((res) => { console.log(res); })
     .catch((err) => { console.log(err); });
   }
--- services/client/src/Pages/CreateAccount/CreateAccount.js
+++ services/client/src/Pages/CreateAccount/CreateAccount.js
@@ -8,10 +8,15 @@ import axios from 'axios';
 class CreateAccount extends Component {
   constructor(props){
     super(props);
+    this.state = {
+      username: null,
+      email: null
+    };
+
   }
   submitCreds = (e) => {
     var credentialJSON = {
-      username: 'myarmo', email: 'mattyarmo@gmail.com'
+      username: this.state.username, email: this.state.email
     };
       axios.post('http://localhost:5001/users', credentialJSON, {
           headers: {
@@ -20,12 +25,24 @@ class CreateAccount extends Component {
       });
   }
 
+  setUsername = (event) => {
+    this.setState({
+      username: event.target.value
+    })
+  }
+
+  setEmail = (event) => {
+    this.setState({
+      email: event.target.value
+    })
+  }
+
   render() {
     return (
       <div className = "login-container">
 
-        <Input placeholder = "Username" className = "login"/>
-        <Input placeholder = "Email" className = "login"/>
+        <Input placeholder = "Username" className = "login" onChange={(e) => this.setUsername(e)}/>
+        <Input placeholder = "Email" className = "login" onChange={(e) => this.setEmail(e)} />
 
         <button onClick={(e) => this.submitCreds(e)} className="login">
           Sign Up
