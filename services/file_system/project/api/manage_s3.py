71e34a7fb62d65b7b32c9da79d694f7657c668ec
JakeVz <jakevz122@gmail.com>
2018-12-05 20:43:16 -0800

    fixed requests

--- services/file_system/project/api/manage_s3.py
+++ services/file_system/project/api/manage_s3.py
@@ -1,18 +1,16 @@
 # services/file_system/project/api/manage_s3.py
 
 import boto3
-from flask import Blueprint, jsonify, request
+from flask import Blueprint, jsonify, request, Flask
 
-s3_blueprint = Blueprint('s3', __name__)
+s3_blueprint = Blueprint('s3', 's3', url_prefix='/s3')
 
-
-@s3_blueprint.route('/s3/upload/<bucket_name>?<file_name>&<user_name>', methods=['POST'])
+@s3_blueprint.route('/upload', methods=['GET'])
 def upload_to_bucket():
     bucket_name = request.args.get('bucket_name', None) 
     file_name = request.args.get('file_name', None) 
     user_name = request.args.get('user_name', None) 
     
-    s3 = boto3.resource('s3')
     client = boto3.client('s3')
     
     client.upload_file(
@@ -21,15 +19,24 @@ def upload_to_bucket():
         Key=file_name + user_name # this is the name of the file that will be stored in the bucket
     )
 
+    response_object = {
+        'status': 'success',
+        'data': {
+            'bucket_name': bucket_name,
+            'file_name': file_name,
+            'user_name': user_name
+        }
+    }
+    return jsonify(response_object), 200
 
-@s3_blueprint.route('/s3/download/<bucket_name>?<file_name>&<orig_file_name>&<user_name>', methods=['GET'])
+
+@s3_blueprint.route('/download', methods=['GET'])
 def download_from_bucket():
     bucket_name = request.args.get('bucket_name', None) 
     file_name = request.args.get('file_name', None) 
     orig_file_name = request.args.get('orig_file_name', None) 
     user_name = request.args.get('user_name', None)
     
-    s3 = boto3.resource('s3')
     client = boto3.client('s3')
     
     client.download_file( 
@@ -37,7 +44,14 @@ def download_from_bucket():
         Filename=file_name, # this is the name you want the downloaded file saved as
         Key=orig_file_name+user_name # this is the key that was used to upload the file. use same format
     )
-
-#if __name__ == '__main__':
-    #upload_to_bucket('csc402uploads', 'Basic_Stats.csv', 'veazey-test')
-    #download_from_bucket('csc402uploads', 'Basic_Stats_new.csv', 'Basic_Stats.csv', 'veazey-test')
+    
+    response_object = {
+        'status': 'success',
+        'data': {
+            'bucket_name': bucket_name,
+            'file_name': file_name,
+            'orig_file_name': orig_file_name,
+            'user_name': user_name
+        }
+    }
+    return jsonify(response_object), 200
