324417b7f2c146035ab29953fa19d379ac02578a
Matt Yarmolich <mattyarmo@gmail.com>
2018-12-05 17:42:52 -0800

    added file upload

--- services/client/src/Pages/Home/Home.js
+++ services/client/src/Pages/Home/Home.js
@@ -1,11 +1,35 @@
 import React, { Component } from 'react';
 import './Home.css';
+import axios from 'axios';
+
+
 
 class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.fileInput = React.createRef();
+  }
+  submitFiles = (e) => {
+    var formData = new FormData();
+    console.log(this.fileInput.current.files[0]);
+    var csvFile = this.fileInput.current.files[0];
+    if(csvFile != null) {
+      formData.append("File", csvFile);
+      axios.post('upload_file', formData, {
+          headers: {
+            'Content-Type': 'multipart/form-data'
+          }
+      });
+    }
+
+  }
+
   render() {
     return (
       <div className = "home-container">
         <div>This is the home component</div>
+        <input type="file" ref={this.fileInput} />
+        <button onClick = {(e) => this.submitFiles(e)} >submit</button>
       </div>
     );
   }
