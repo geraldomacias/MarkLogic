4a465693bf7cf6eef2549854cb816c5218444f73
= <=>
2019-01-14 20:41:42 -0800

    Added tests and routes for /users/register and /users/login

--- services/users/project/config.py
+++ services/users/project/config.py
@@ -7,15 +7,18 @@ class BaseConfig:
     TESTING = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = 'my_precious'
+    BCRYPT_LOG_ROUNDS = 13
 
 class DevelopmentConfig(BaseConfig):
     """Development Configuration"""
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    BCRYPT_LOG_ROUNDS = 4
 
 class TestingConfig(BaseConfig):
     """Testing Configuration"""
     TESTING = True
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_TEST_URL')
+    BCRYPT_LOG_ROUNDS = 4
 
 class ProductionConfig(BaseConfig):
     """Production Configuration"""
