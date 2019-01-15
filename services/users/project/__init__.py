0d6930fb46a6f227ea9d6f27a444340b7d8d169d
geraldomacias <macias.g@outlook.com>
2019-01-15 09:31:01 -0800

    First commit

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -1,10 +1,14 @@
 # services/users/project/__init__.py
 
+# QUESTION: When installing flask-s3, how do I ensure
+# build_and_run.sh also does this?
+
 import os
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+from flask_s3 import FlaskS3
 
 # instantiate the db
 db = SQLAlchemy()
@@ -13,7 +17,7 @@ def create_app(script_info=None):
 
     # instantiate the app
     app = Flask(__name__)
-    
+
     # enable CORS
     CORS(app)  # new
 
@@ -28,9 +32,15 @@ def create_app(script_info=None):
     from project.api.users import users_blueprint
     app.register_blueprint(users_blueprint)
 
+    # enable s3
+    app.config['FLASKS3_BUCKET_NAME'] = 'bucket_name'
+    s3 = FlaskS3(app)
+
+
     # shell context for flask cli
+    # this is used to register the app and db to the shell
     @app.shell_context_processor
     def ctx():
         return {'app': app, 'db': db}
-    
+
     return app
