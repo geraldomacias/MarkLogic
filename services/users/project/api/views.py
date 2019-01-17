fa51442d6d74e332d516b2f55df2b46f95a1c06a
Spencer Schurk <spencer@joyrun.com>
2019-01-17 15:36:15 -0800

    Added blacklisting tokens and logout api

--- services/users/project/api/views.py
+++ services/users/project/api/views.py
@@ -4,7 +4,7 @@ from flask import Blueprint, request, make_response, jsonify
 from flask.views import MethodView
 
 from project import bcrypt, db
-from project.api.models import User 
+from project.api.models import User , BlacklistToken
 
 users_blueprint = Blueprint('users', __name__)
 
@@ -134,10 +134,55 @@ class UserAPI(MethodView):
             }
             return make_response(jsonify(responseObject)), 401
 
+class LogoutAPI(MethodView):
+    """
+    Logout Resource
+    """
+    def post(self):
+        # get auth token
+        auth_header = request.headers.get('Authorization')
+        if auth_header:
+            auth_token = auth_header.split(" ")[1]
+        else:
+            auth_token = ''
+        if auth_token:
+            resp = User.decode_auth_token(auth_token)
+            if not isinstance(resp, str):
+                # mark the token as blacklisted
+                blacklist_token = BlacklistToken(token=auth_token)
+                try:
+                    # insert the token
+                    db.session.add(blacklist_token)
+                    db.session.commit()
+                    responseObject = {
+                        'status': 'success',
+                        'message': 'Successfully logged out.'
+                    }
+                    return make_response(jsonify(responseObject)), 200
+                except Exception as e:
+                    responseObject = {
+                        'status': 'fail',
+                        'message': e
+                    }
+                    return make_response(jsonify(responseObject)), 401
+            else:
+                responseObject = {
+                    'status': 'fail',
+                    'message': resp
+                }
+                return make_response(jsonify(responseObject)), 401
+        else:
+            responseObject = {
+                'status': 'fail',
+                'message': 'Provide a valid auth token.'
+            }
+            return make_response(jsonify(responseObject)), 403
+
 # define the API resources
 registration_view = RegisterAPI.as_view('register_api')
 login_view = LoginAPI.as_view('login_api')
 user_view = UserAPI.as_view('user_api')
+logout_view = LogoutAPI.as_view('logout_api')
 
 # add Rules for API Endpoints
 users_blueprint.add_url_rule(
@@ -154,4 +199,9 @@ users_blueprint.add_url_rule(
     '/users/status',
     view_func=user_view,
     methods=['GET']
+)
+users_blueprint.add_url_rule(
+    '/users/logout',
+    view_func=logout_view,
+    methods=['POST']
 )
--- services/users/project/tests/test_users.py
+++ services/users/project/tests/test_users.py
@@ -5,7 +5,7 @@ import unittest
 import time
 
 from project import db
-from project.api.models import User
+from project.api.models import User, BlacklistToken
 from project.tests.base import BaseTestCase
 
 def add_user(email, password):
@@ -28,7 +28,7 @@ class TestUserModel(BaseTestCase):
         user = add_user('test@test.com', 'test')
         auth_token = user.encode_auth_token(user.id)
         self.assertTrue(isinstance(auth_token, bytes))
-        self.assertTrue(User.decode_auth_token(auth_token) == 1)
+        self.assertTrue(User.decode_auth_token(auth_token.decode("utf-8")) == 1)
 
 
 class TestUserService(BaseTestCase):
@@ -304,7 +304,7 @@ class TestUserService(BaseTestCase):
                 '/users/logout',
                 headers=dict(
                     Authorization='Bearer ' + json.loads(
-                        resp_login.data_decode()
+                        resp_login.data.decode()
                     )['auth_token']
                 )
             )
@@ -312,6 +312,88 @@ class TestUserService(BaseTestCase):
             self.assertTrue(data['status'] == 'fail')
             self.assertTrue(
                 data['message'] == 'Signature expired. Please log in again.')
+            self.assertEqual(response.status_code, 401)
+    
+    def test_valid_blacklisted_token_logout(self):
+        """Test for logout after valid token gets blacklisted"""
+        with self.client:
+            # user registration
+            resp_register = self.client.post(
+                '/users/register',
+                data=json.dumps(dict(
+                    email='joe@gmail.com',
+                    password='123456'
+                )),
+                content_type='application/json'
+            )
+            data_register = json.loads(resp_register.data.decode())
+            self.assertTrue(data_register['status'] == 'success')
+            self.assertTrue(
+                data_register['message'] == 'Successfully registered.')
+            self.assertTrue(data_register['auth_token'])
+            self.assertTrue(resp_register.content_type == 'application/json')
+            self.assertEqual(resp_register.status_code, 201)
+            # user login
+            resp_login = self.client.post(
+                '/users/login',
+                data=json.dumps(dict(
+                    email='joe@gmail.com',
+                    password='123456'
+                )),
+                content_type='application/json'
+            )
+            data_login = json.loads(resp_login.data.decode())
+            self.assertTrue(data_login['status'] == 'success')
+            self.assertTrue(data_login['message'] == 'Successfully logged in.')
+            self.assertTrue(data_login['auth_token'])
+            self.assertTrue(resp_login.content_type == 'application/json')
+            self.assertEqual(resp_login.status_code, 200)
+            # blacklist a valid token
+            blacklist_token = BlacklistToken(
+                token=json.loads(resp_login.data.decode())['auth_token'])
+            db.session.add(blacklist_token)
+            db.session.commit()
+            # blacklisted valid token logout
+            response = self.client.post(
+                '/users/logout',
+                headers=dict(
+                    Authorization="Bearer " + json.loads(
+                        resp_login.data.decode()
+                    )['auth_token']
+                )
+            )
+            data = json.loads(response.data.decode())
+            self.assertTrue(data['status'] == 'fail')
+            self.assertTrue(data['message'] == 'Token blacklisted. Please log in again.')
+            self.assertEqual(response.status_code, 401)
+
+    def test_valid_blacklisted_token_user(self):
+        """Test for user status with a blacklsited valid token"""
+        with self.client:
+            resp_register = self.client.post(
+                '/users/register',
+                data=json.dumps(dict(
+                    email='joe@gmail.com',
+                    password='123456'
+                )),
+                content_type='application/json'
+            )
+            # blacklist a valid token
+            blacklist_token = BlacklistToken(
+                token=json.loads(resp_register.data.decode())['auth_token'])
+            db.session.add(blacklist_token)
+            db.session.commit()
+            response = self.client.get(
+                '/users/status',
+                headers=dict(
+                    Authorization='Bearer ' + json.loads(
+                        resp_register.data.decode()
+                    )['auth_token']
+                )
+            )
+            data=json.loads(response.data.decode())
+            self.assertTrue(data['status'] == 'fail')
+            self.assertTrue(data['message'] == 'Token blacklisted. Please log in again.')
             self.assertEqual(response.status_code, 401)
 
 if __name__ == '__main__':
