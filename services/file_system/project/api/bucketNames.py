00cc509e2637ec16d07aabce66050f7c6b22b6c3
EC2 Default User <ec2-user@ip-172-31-24-230.us-west-2.compute.internal>
2019-01-24 08:28:59 +0000

    S3 should be working -- worked with my test files

---
+++ services/file_system/project/api/bucketNames.py
@@ -0,0 +1,5 @@
+# services/file_system/project/api/bucketNames.py
+
+S3UPLOAD = "capstone.upload"
+S3CLASSIFIED = "capstone.classified"
+S3STATIC = "capstone.static"
--- services/file_system/project/api/manage_s3.py
+++ services/file_system/project/api/manage_s3.py
@@ -2,15 +2,27 @@
 
 import boto3
 from flask import Blueprint, jsonify, request, Flask
+import bucketNames
 
 s3_blueprint = Blueprint('s3', 's3', url_prefix='/s3')
 
 @s3_blueprint.route('/upload', methods=['GET'])
-def upload_to_bucket():
-    bucket_name = request.args.get('bucket_name', None) 
-    file_name = request.args.get('file_name', None) 
-    user_name = request.args.get('user_name', None) 
-    
+def upload_to_bucket(*argv):
+    if (len(argv) == 0): # This is for prod.
+        file_name = request.args.get('file_name', None) 
+        user_name = request.args.get('user_name', None) 
+    elif (len(argv) == 2): # This is for testing (inputting own values)
+        file_name = argv[0]
+        user_name = argv[1]
+    else:
+        print("Incorrect Function call.")
+        return
+
+    if (requests.args.get('bucket_name', None) == 'uploads'):
+        bucket_name = bucketNames.S3UPLOAD
+    else:
+        bucket_name = bucketNames.S3CLASSIFIED
+
     client = boto3.client('s3')
     
     client.upload_file(
@@ -32,11 +44,11 @@ def upload_to_bucket():
 
 @s3_blueprint.route('/download', methods=['GET'])
 def download_from_bucket():
-    bucket_name = request.args.get('bucket_name', None) 
     file_name = request.args.get('file_name', None) 
     orig_file_name = request.args.get('orig_file_name', None) 
     user_name = request.args.get('user_name', None)
     
+    bucket_name = bucketNames.S3CLASSIFIED
     client = boto3.client('s3')
     
     client.download_file( 
@@ -55,3 +67,9 @@ def download_from_bucket():
         }
     }
     return jsonify(response_object), 200
+
+# For testing
+#if __name__ == '__main__':
+   #upload_to_bucket('Basic_Stats.csv', 'veazey-test')
+   #download_from_bucket('csc402uploads', 'Basic_Stats_new.csv', 'Basic_Stats.csv', 'veazey-test')
+
