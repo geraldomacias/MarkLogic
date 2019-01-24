d470aa54757ebf0fff065301ca6ffb2737368c63
EC2 Default User <ec2-user@ip-172-31-24-230.us-west-2.compute.internal>
2019-01-24 08:28:59 +0000

    S3 should be working -- worked with my test files

--- services/file_system/project/api/constants.py
+++ services/file_system/project/api/constants.py
@@ -3,3 +3,6 @@
 S3UPLOAD = "capstone.upload"
 S3CLASSIFIED = "capstone.classified"
 S3STATIC = "capstone.static"
+
+# UPLOAD_FOLDER = '/uploadFoler/'
+UPLOAD_FOLDER = '/home/ec2-user/MarkLogic/services/file_system/project/api/uploadFolder'
--- services/file_system/project/api/manage_s3.py
+++ services/file_system/project/api/manage_s3.py
@@ -1,42 +1,54 @@
 # services/file_system/project/api/manage_s3.py
 
+import os
 import boto3
 from flask import Blueprint, jsonify, request, Flask
-import bucketNames
+import constants
 
 s3_blueprint = Blueprint('s3', 's3', url_prefix='/s3')
 
-@s3_blueprint.route('/upload', methods=['GET'])
-def upload_to_bucket(*argv):
-    if (len(argv) == 0): # This is for prod.
-        file_name = request.args.get('file_name', None) 
-        user_id = request.args.get('user_id', None) 
-    elif (len(argv) == 2): # This is for testing (inputting own values)
-        file_name = argv[0]
-        user_id = argv[1]
-    else:
-        print("Incorrect Function call.")
-        return
+
+@s3_blueprint.route('/upload', methods=['POST'])
+def upload_to_bucket():
+    response_object = {
+        'status': 'success'
+    }
+
+    # TODO: need to change to make it get user_id from token
+    user_id = request.args.get('user_id', None) 
+
+    if 'file' not in request.files:
+        print('no file passed')
+        return jsonify(response_object), 400
+
+    file_passed = request.files['file'] 
+    file_name = file_passed.filename
+
+    if (file_name == ''):
+        print('no file selected')
+        return jsonify(response_object), 400
+
+    # saves to a temp location
+    file_passed.save(os.path.join(contants.UPLOAD_FOLDER, file_name))
 
     if (requests.args.get('bucket_name', None) == 'uploads'):
-        bucket_name = bucketNames.S3UPLOAD
+        bucket_name = constants.S3UPLOAD
         file_type = "uploads"
     else:
-        bucket_name = bucketNames.S3CLASSIFIED
+        bucket_name = constants.S3CLASSIFIED
         file_type = "classified"
 
     client = boto3.client('s3')
     key_name = file_name + '.' + user_id + '.' + file_type # this is the name of the file that will be stored in the bucket
-    
+ 
     client.upload_file(
         Bucket=bucket_name, # name of the bucket
-        Filename=file_name, # this is the name of the file that is being uploaded
+        Filename=constants.UPLOAD_FOLDER + file_name, # this is the name of the file that is being uploaded
         Key=key_name
     )
 
     # TODO: Check to see if user and file exist in DB
     # TODO: if yes, update with URL. else create new item
-
 
     response_object = {
         'status': 'success',
@@ -57,10 +69,10 @@ def download_from_bucket():
     user_id = request.args.get('user_id', None)
 
     if (requests.args.get('bucket_name', None) == 'uploads'):
-        bucket_name = bucketNames.S3UPLOAD
+        bucket_name = constants.S3UPLOAD
         file_type = "uploads"
     else:
-        bucket_name = bucketNames.S3CLASSIFIED
+        bucket_name = constants.S3CLASSIFIED
         file_type = "classified"
 
     client = boto3.client('s3')
