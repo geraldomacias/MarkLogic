2e5fddec12d9ec4b6f407714867faa0c3756606d
EC2 Default User <ec2-user@ip-172-31-24-230.us-west-2.compute.internal>
2019-01-29 17:26:36 +0000

    made calling requests better

--- services/file_system/project/api/manage_s3.py
+++ services/file_system/project/api/manage_s3.py
@@ -2,101 +2,119 @@
 
 import os
 import boto3
-from flask import Blueprint, jsonify, request, Flask
 import constants
+from flask import Blueprint, jsonify, request, Flask, make_response
+from flask.views import MethodView
+from flask_cors import CORS
 
-s3_blueprint = Blueprint('s3', 's3', url_prefix='/s3')
-
-
-@s3_blueprint.route('/upload', methods=['POST'])
-def upload_to_bucket():
-    response_object = {
-        'status': 'success'
-    }
-
-    # TODO: need to change to make it get user_id from token
-    user_id = request.args.get('user_id', None) 
-
-    if 'file' not in request.files:
-        print('no file passed')
-        return jsonify(response_object), 400
-
-    file_passed = request.files['file'] 
-    file_name = file_passed.filename
-
-    if (file_name == ''):
-        print('no file selected')
-        return jsonify(response_object), 400
-
-    # saves to a temp location
-    file_passed.save(os.path.join(contants.UPLOAD_FOLDER, file_name))
-
-    if (requests.args.get('bucket_name', None) == 'uploads'):
-        bucket_name = constants.S3UPLOAD
-        file_type = "uploads"
-    else:
-        bucket_name = constants.S3CLASSIFIED
-        file_type = "classified"
-
-    client = boto3.client('s3')
-    key_name = file_name + '.' + user_id + '.' + file_type # this is the name of the file that will be stored in the bucket
- 
-    client.upload_file(
-        Bucket=bucket_name, # name of the bucket
-        Filename=constants.UPLOAD_FOLDER + file_name, # this is the name of the file that is being uploaded
-        Key=key_name
-    )
-
-    # TODO: Check to see if user and file exist in DB
-    # TODO: if yes, update with URL. else create new item
-
-    response_object = {
-        'status': 'success',
-        'data': {
-            'bucket_name': bucket_name,
-            'file_name': file_name,
-            'user_id': user_id,
-            'key_name': key_name
-        }
-    }
-    return jsonify(response_object), 200
+# from project import bcrypt, db
+# from project.api.models import User , BlacklistToken
 
 
-@s3_blueprint.route('/download', methods=['GET'])
-def download_from_bucket():
-    file_name = request.args.get('file_name', None) 
-    orig_file_name = request.args.get('orig_file_name', None) 
-    user_id = request.args.get('user_id', None)
+s3_blueprint = Blueprint('s3', __name__)
 
-    if (requests.args.get('bucket_name', None) == 'uploads'):
-        bucket_name = constants.S3UPLOAD
-        file_type = "uploads"
-    else:
-        bucket_name = constants.S3CLASSIFIED
-        file_type = "classified"
+CORS(s3_blueprint)
 
-    client = boto3.client('s3')
-    key_name = orig_file_name + '.' + user_id + '.' + file_type # this is the name of the file that was stored in the bucket
-
-    client.download_file( 
-        Bucket=bucket_name, # name of the bucket
-        Filename=file_name, # this is the name you want the downloaded file saved as
-        Key=key_name
-    )
+class UploadAPI(MethodView):
     
-    response_object = {
-        'status': 'success',
-        'data': {
-            'bucket_name': bucket_name,
-            'file_name': file_name,
-            'orig_file_name': orig_file_name,
-            'user_id': user_id
+    def post(self):
+        response_object = {
+            'status': 'success'
         }
-    }
-    return jsonify(response_object), 200
 
-# For testing
-#if __name__ == '__main__':
-   #upload_to_bucket('Basic_Stats.csv', 'veazey-test')
-   #download_from_bucket('csc402uploads', 'Basic_Stats_new.csv', 'Basic_Stats.csv', 'veazey-test')
+        # TODO: need to change to make it get user_id from token
+        user_id = request.args.get('user_id', None) 
+
+        if 'file' not in request.files:
+            print('no file passed')
+            return jsonify(response_object), 400
+
+        file_passed = request.files['file'] 
+        file_name = file_passed.filename
+
+        if (file_name == ''):
+            print('no file selected')
+            return jsonify(response_object), 400
+
+        # saves to a temp location
+        file_passed.save(os.path.join(contants.UPLOAD_FOLDER, file_name))
+
+        if (requests.args.get('bucket_name', None) == 'uploads'):
+            bucket_name = constants.S3UPLOAD
+            file_type = "uploads"
+        else:
+            bucket_name = constants.S3CLASSIFIED
+            file_type = "classified"
+
+        client = boto3.client('s3')
+        key_name = file_name + '.' + user_id + '.' + file_type # this is the name of the file that will be stored in the bucket
+    
+        client.upload_file(
+            Bucket=bucket_name, # name of the bucket
+            Filename=constants.UPLOAD_FOLDER + file_name, # this is the name of the file that is being uploaded
+            Key=key_name
+        )
+
+        # TODO: Check to see if user and file exist in DB
+        # TODO: if yes, update with URL. else create new item
+
+        response_object = {
+            'status': 'success',
+            'data': {
+                'bucket_name': bucket_name,
+                'file_name': file_name,
+                'user_id': user_id,
+                'key_name': key_name
+            }
+        }
+        return make_response(jsonify(response_object)), 200
+
+
+class DownloadAPI(MethodView):
+    
+    def get(self):
+        file_name = request.args.get('file_name', None) 
+        orig_file_name = request.args.get('orig_file_name', None) 
+        user_id = request.args.get('user_id', None)
+
+        if (requests.args.get('bucket_name', None) == 'uploads'):
+            bucket_name = constants.S3UPLOAD
+            file_type = "uploads"
+        else:
+            bucket_name = constants.S3CLASSIFIED
+            file_type = "classified"
+
+        client = boto3.client('s3')
+        key_name = orig_file_name + '.' + user_id + '.' + file_type # this is the name of the file that was stored in the bucket
+
+        client.download_file( 
+            Bucket=bucket_name, # name of the bucket
+            Filename=file_name, # this is the name you want the downloaded file saved as
+            Key=key_name
+        )
+        
+        response_object = {
+            'status': 'success',
+            'data': {
+                'bucket_name': bucket_name,
+                'file_name': file_name,
+                'orig_file_name': orig_file_name,
+                'user_id': user_id
+            }
+        }
+        return make_response(jsonify(response_object)), 200
+
+upload_view = UploadAPI.as_view('upload_api')
+download_view = DownloadAPI.as_view('download_api')
+
+s3_blueprint.add_url_rule(
+    '/users/upload',
+    view_func=upload_view,
+    methods=['POST']
+)
+s3_blueprint.add_url_rule(
+    '/users/download',
+    view_func=download_view,
+    methods=['GET']
+)
 
