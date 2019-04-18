71d999805400b1bf4b93942151ad7b65367674dc
= <=>
2019-04-17 19:18:45 -0700

    might've fixed a thing

--- recreate_db.sh
+++ recreate_db.sh
@@ -1,3 +1,3 @@
-docker-compose -f docker-compose-dev.yml run users python manage.py recreate-db
-docker-compose -f docker-compose-dev.yml run file_system python manage.py recreate-db
-docker-compose -f docker-compose-dev.yml run machine_learning python manage.py recreate-db
+docker-compose -f docker-compose-dev.yml run users python manage.py recreate_db
+docker-compose -f docker-compose-dev.yml run file_system python manage.py recreate_db
+docker-compose -f docker-compose-dev.yml run machine_learning python manage.py recreate_db
